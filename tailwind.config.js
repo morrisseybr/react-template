2cc65c23764cc51ad0cc629d8ad1014416ca9c1d
Samuel Morrissey <samuel.morrissey@outlook.com>
2022-02-09 20:22:18 -0300

    Configurações Storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+const path = require("path");
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["index.html", path.join(__dirname, "./src/**/*.(js|jsx|ts|tsx)")],
   theme: {
     extend: {},
   },
